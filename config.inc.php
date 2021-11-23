1e05495642b08f1d649fcd112b705c0a3ee4be7e
Jeremias Rauwolf <82889934+jerreeey@users.noreply.github.com>
2021-11-23 01:20:27 +0100

    Update config.inc database connection

--- config.inc.php
+++ config.inc.php
@@ -1,6 +1,6 @@
 <?php
 //define database connection variables
-define("DB_HOST", "mysql5");
-define("DB_NAME","db_flock-1724_1");
-define("DB_USER","flock-1724");
-define("DB_PW","Gdxy?JgcCu59");
+define("DB_HOST", #db host);
+define("DB_NAME",#db name);
+define("DB_USER",#db user);
+define("DB_PW",#db password);
